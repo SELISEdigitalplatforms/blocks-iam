e5edca143d45a6dd21728fdf114274ce1ebff9f6
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-05-12 19:04:51 +0600

    oidic driver code add

---
+++ server/SeliseBlocks.OIDC.Driver/OidcDriverServiceExtention.cs
@@ -0,0 +1,18 @@
+﻿using Authentication.DomainService.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeliseBlocks.OIDC.Driver
+{
+    public static class OidcDriverServiceExtention
+    {
+        public static void RegisterBlocksOidcDriverService(this IServiceCollection services)
+        {
+            services.RegisterAllServices();
+            services.AddSingleton<IOidcDriverService, OidcDriverService>();
+        }
+    }
+
+}
