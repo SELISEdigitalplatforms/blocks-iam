53e2fe68f7bda49de96aef64a5b09019251ed64a
mostafizSelise <mostafizur.rahman@selisegroup.com>
2026-05-13 18:57:21 +0600

    updated

--- server/Identifier.DomainService/Subscription/Services/ISubscriptionRepository.cs
+++
@@ -1,9 +0,0 @@
-using Identifier.DomainService.Entities;
-
-namespace Identifier.DomainService.Subscription.Services
-{
-    public interface ISubscriptionRepository
-    {
-        public Task<List<ResourceLimit>> GetSubscriptionsAsync();
-    }
-}
