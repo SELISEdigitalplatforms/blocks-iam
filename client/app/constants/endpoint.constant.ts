cb7158f985f4c3dcd5f2fef0b1af79f281ee049c
afiapreety <afia.preety@selisegroup.com>
2026-05-14 21:19:43 +0600

    fix: remove api bases

--- client/app/constants/endpoint.constant.ts
+++ client/app/constants/endpoint.constant.ts
@@ -1,19 +1,1 @@
-import { getRuntimeEnv } from "@/lib/runtime-env";
-
 export const BLOCKS_OS_BASE_URL = "https://dev-os.blocksdevelopers.com";
-
-export const API_BASES = {
-  COMMUNICATION: "/api",
-  CLOUD_CONFIGURATION: "/api",
-  UDS: "/api",
-  UILM: "/api",
-  UTILITIES: "/api",
-  CLOUD_BUILD: "/api",
-  IDP: getRuntimeEnv("BLOCKS_IDP_BASE_URL") + "/api",  
-  IDENTIFIER: "/api",
-  LMT: "/api",
-  MFA: "/api",
-  ALERT: "/api",
-  AI: "/api",
-  STUDIO: "/api",
-} as const;
