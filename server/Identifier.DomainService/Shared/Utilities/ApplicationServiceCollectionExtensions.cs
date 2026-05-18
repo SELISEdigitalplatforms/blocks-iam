f9f86fca131705d68468880e858fd19b61ad6d63
mostafizSelise <mostafizur.rahman@selisegroup.com>
2026-05-19 03:57:04 +0600

    Remove unused package references and clean up service registration in ApplicationServiceCollectionExtensions

--- server/Identifier.DomainService/Shared/Utilities/ApplicationServiceCollectionExtensions.cs
+++ server/Identifier.DomainService/Shared/Utilities/ApplicationServiceCollectionExtensions.cs
@@ -1,13 +1,5 @@
-using Blocks.Extension.DependencyInjection;
 using Identifier.DomainService.Projects;
-using Identifier.DomainService.Shared.Utilities;
-using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
-using Storage.DomainService.Shared.Services;
-using DomainService.Storage;
-using DomainService.Storage.Validators;
-using Storage.DomainService.Storage;
-using Storage.DomainService.Storage.Validators;
 
 namespace Identifier.DomainService.Shared
 {
@@ -21,14 +13,6 @@ namespace Identifier.DomainService.Shared
             services.AddSingleton<IProjectRepository, ProjectRepository>();
 
             // Drivers
-            services.AddSingleton<DmsArtifactBuilderFactory>();
-            services.AddTransient<IValidator<UpdateFileRequest>, UpdateFileRequestValidator>(); 
-            services.AddTransient<AwsS3CompatibleStorageService>();
-            services.AddSingleton<FileArtifactBuilder>();
-            services.AddSingleton<FolderArtifactBuilder>();
-
-            services.RegisterBlocksStorageServices();
-            services.RegisterBlocksMailService();
         }
     }
 }
