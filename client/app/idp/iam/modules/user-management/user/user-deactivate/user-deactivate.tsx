11a997e152fee6fd6d6d0b59bfc544a483c2be1b
UDay <uday.dev.bd@gmail.com>
2026-05-24 18:42:38 +0600

    refactor: remove unused projectKey from activation payloads and clean up code formatting

--- client/app/idp/iam/modules/user-management/user/user-deactivate/user-deactivate.tsx
+++ client/app/idp/iam/modules/user-management/user/user-deactivate/user-deactivate.tsx
@@ -16,22 +16,21 @@ export const UserDeactivate = ({
   open,
   setOpen,
 }: UserDeactivateProps) => {
-  const tenantId = useProjectStore().selectedProject?.tenantId || "";
   const { mutateAsync, isPending } = useAccountDeactivate();
   const onClickHandler = async () => {
     try {
-      const res = await mutateAsync({ projectKey: tenantId, userId });
+      const res = await mutateAsync({ userId });
       if (res.isSuccess) {
-        showSuccessToast(
-          { description: "User has been deactivated successfully." }
-        );
+        showSuccessToast({
+          description: "User has been deactivated successfully.",
+        });
+      } else {
+        showErrorToast({ errors: res.errors });
       }
-      else { showErrorToast({ errors: res.errors }); }
     } catch (error) {
       if (isErrorWithErrors(error)) {
         showErrorToast({ errors: error.errors });
-      }
-      else {
+      } else {
         showErrorToast({ errors: "Something went wrong" });
       }
     } finally {
