cdc2c2d2a736719dd1484c4b1cede537a0fe82cb
afiapreety <afia.preety@selisegroup.com>
2026-05-26 00:15:16 +0600

    feat: new gradiant

--- client/app/components/ui-kits/button/button.tsx
+++ client/app/components/ui-kits/button/button.tsx
@@ -10,6 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        primary: "blocks-gradient text-white hover:opacity-90 disabled:opacity-50",
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         "destructive-outline": "border border-error  text-error hover:bg-destructive/20 ",
