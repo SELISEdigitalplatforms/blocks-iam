3f30968de92c7142ad2357f2574055bfaaab65b4
mostafizSelise <mostafizur.rahman@selisegroup.com>
2026-06-10 15:09:08 +0600

    fix: change SameSite cookie attribute from Lax to Strict

--- server/Authentication.DomainService/Authentication/AuthorizationFlowService.cs
+++ server/Authentication.DomainService/Authentication/AuthorizationFlowService.cs
@@ -1232,7 +1232,7 @@ namespace Authentication.DomainService.Authentication
                 Domain = adjustedCookieDomain,
                 HttpOnly = true,
                 Secure = true,
-                SameSite = isLocal ? SameSiteMode.None : SameSiteMode.Lax,
+                SameSite = isLocal ? SameSiteMode.None : SameSiteMode.Strict,
                 Path = "/",
                 Expires = absoluteExpiry == default ? DateTime.UtcNow.Add(GetIdpSessionAbsoluteTimeout()) : absoluteExpiry
             });
