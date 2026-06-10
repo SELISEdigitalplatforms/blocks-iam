ba67fe82e23d36b7dd1978ddf46613c481e26a25
mostafizSelise <mostafizur.rahman@selisegroup.com>
2026-06-11 03:28:02 +0600

    Add OIDC configuration properties to IdentityConfiguration and IamConfiguration

--- server/Iam.DomainService/Shared/Dtos/IamConfiguration.cs
+++ server/Iam.DomainService/Shared/Dtos/IamConfiguration.cs
@@ -11,6 +11,8 @@ namespace Iam.DomainService.Dtos
         public string AccountActivationPath { get; set; }
         public string AccountVerificationPath { get; set; }
         public string RecoverAccountPath { get; set; }
+        public bool IsOidcEnabled { get; set; } = false;
+        public string AccountActionBaseUrl { get; set; }
         public int ActivationUrlLifetimeInMinutes { get; set; } = 60 * 24;
         public int RecoverAccountUrlLifetimeInMinutes { get; set; } = 10;
         public bool LogoutOnPasswordChange { get; set; } = true;
