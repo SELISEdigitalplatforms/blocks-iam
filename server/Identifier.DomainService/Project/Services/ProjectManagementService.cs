f2ff725231ef1ad6bad954dfea548987f0d8c01e
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-06-18 14:45:16 +0600

    updated to latest genesis package

--- server/Identifier.DomainService/Project/Services/ProjectManagementService.cs
+++ server/Identifier.DomainService/Project/Services/ProjectManagementService.cs
@@ -48,7 +48,7 @@ namespace Identifier.DomainService.Projects
                 CreatedDate = repoProject.CreatedDate,
                 LastUpdatedDate = repoProject.LastUpdatedDate,
                 LastUpdatedBy = repoProject.LastUpdatedBy,
-                OrganizationIds = repoProject.OrganizationIds,
+               // OrganizationIds = repoProject.OrganizationIds,
                 CreatedBy = repoProject.CreatedBy,
                 Tags = repoProject.Tags,
                 TenantId = repoProject.TenantId,
