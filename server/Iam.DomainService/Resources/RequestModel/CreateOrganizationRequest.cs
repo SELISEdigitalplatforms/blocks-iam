5bff1bbc09d39ccde91f9a5c86384b358c54d450
mostafizSelise <mostafizur.rahman@selisegroup.com>
2026-06-18 19:00:55 +0600

    feat: enhance organization and request models with additional fields for better data handling

--- server/Iam.DomainService/Resources/RequestModel/CreateOrganizationRequest.cs
+++ server/Iam.DomainService/Resources/RequestModel/CreateOrganizationRequest.cs
@@ -1,12 +1,20 @@
+using Iam.DomainService.Shared.Entities;
+
 namespace Iam.DomainService.Resources
 {
     public class CreateOrganizationRequest
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
         public List<string> DefaultRoleForMembers { get; set; } = new List<string>();
         public List<string> DefaultPermissionsForMembers { get; set; } = new List<string>();
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? WebsiteUrl { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+        public Dictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
+
         public CreatedFrom CreatedFrom { get; set; } = 0;
-        public string? Description { get; set; }
     }
 
     public enum CreatedFrom
