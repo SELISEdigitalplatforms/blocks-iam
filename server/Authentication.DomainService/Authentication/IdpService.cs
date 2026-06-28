7571360394a88379765ac62fde301069241d9626
afiapreety <afia.preety@selisegroup.com>
2026-06-28 13:37:56 +0600

    fix: undo local changes

--- server/Authentication.DomainService/Authentication/IdpService.cs
+++ server/Authentication.DomainService/Authentication/IdpService.cs
@@ -194,8 +194,8 @@ namespace Authentication.DomainService.Authentication
                 }
 
                 // Exchange authorization code for tokens at IdP
-                // var tokenEndpoint = identityProvider.TokenUrl;
-                var tokenEndpoint = "https://dev-iam.blocksdevelopers.com:5001/api/oidc/token?tenant_id=***REMOVED***";
+                var tokenEndpoint = identityProvider.TokenUrl;
+                // var tokenEndpoint = "https://dev-iam.blocksdevelopers.com:5001/api/oidc/token?tenant_id=***REMOVED***";
  
                 var form = new Dictionary<string, string>
                 {
