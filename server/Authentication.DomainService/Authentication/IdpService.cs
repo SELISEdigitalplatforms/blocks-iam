da8a844a69ecc1f0f52e1a5b4b574b6b41f3f919
afiapreety <afia.preety@selisegroup.com>
2026-06-28 13:34:47 +0600

    ci: retrigger build

--- server/Authentication.DomainService/Authentication/IdpService.cs
+++ server/Authentication.DomainService/Authentication/IdpService.cs
@@ -194,7 +194,9 @@ namespace Authentication.DomainService.Authentication
                 }
 
                 // Exchange authorization code for tokens at IdP
-                var tokenEndpoint = identityProvider.TokenUrl;
+                // var tokenEndpoint = identityProvider.TokenUrl;
+                var tokenEndpoint = "https://dev-iam.blocksdevelopers.com:5001/api/oidc/token?tenant_id=f080a1bea04280a72149fd689d50a48c";
+ 
                 var form = new Dictionary<string, string>
                 {
                     { "grant_type", "authorization_code" },
