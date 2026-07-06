8f2261fe5717c5df922d57b71a3d676a1d4d59f8
afiapreety <afia.preety@selisegroup.com>
2026-07-06 21:10:08 +0600

    fix: mfa changes removing projectkey

--- client/app/idp/mfa/constants/endpoint.constant.ts
+++ client/app/idp/mfa/constants/endpoint.constant.ts
@@ -16,7 +16,8 @@ export const MFA_ENDPOINTS = {
   GENERATE_OTP: `/api${MFA_SUBPATH}/Generate`,
   CONFIGURE_USER_MFA: `/api${MANAGEMENT_SUBPATH}/ConfigureUserMfa`,
   SETUP_TOTP: `/api${MFA_SUBPATH}/Totp/Setup`,
-  VERIFY_OTP: `/api${MFA_SUBPATH}/Totp/Verify-setup`,
+  VERIFY_OTP: `/api${MFA_SUBPATH}/Verify`,
+  VERIFY_TOTP_SETUP: `/api${MFA_SUBPATH}/Totp/Verify-setup`,
   RESEND_OTP: `/api${MFA_SUBPATH}/Resend`,
   DISABLE_MFA: `/api${MFA_SUBPATH}/Disable`,
 } as const;
--- client/app/idp/mfa/models/mfa.model.ts
+++ client/app/idp/mfa/models/mfa.model.ts
@@ -28,7 +28,6 @@ export interface IConfigureUserMFAPayload {
   userId: string;
   mfaEnabled: boolean;
   userMfaType: number;
-  projectKey: string;
 }
 export interface IConfigureUserMFAResponse {
   errors: unknown | null;
