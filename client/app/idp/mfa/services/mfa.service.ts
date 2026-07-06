fdddceebd6365a651dbf3fe19bdffd3b47717ec7
afiapreety <afia.preety@selisegroup.com>
2026-07-06 11:23:03 +0600

    fix: mfa get/save, saveMFAConfiguration, setupUserTotp , verifyOtp, disableMFA from iam

--- client/app/idp/mfa/services/mfa.service.ts
+++ client/app/idp/mfa/services/mfa.service.ts
@@ -1,7 +1,7 @@
 import { serviceInstances } from "@/lib/http-client";
 import { getRuntimeEnv } from "@/lib/runtime-env";
 
-const toLogicUrl = (path: string) => `${getRuntimeEnv("BLOCKS_LOGIC_BASE_URL")}${path}`;
+const toIamUrl = (path: string) => `${getRuntimeEnv("BLOCKS_IAM_BASE_URL")}${path}`;
 import {
   IGenerateUserMFA_OtpPayload,
   IGenerateUserMFA_OtpResponse,
@@ -23,17 +23,17 @@ import { MFA_CONFIG_ENDPOINTS, MFA_ENDPOINTS } from "../constants/endpoint.const
 
 export class MFAService {
   getConfigurations(): Promise<IGetConfigurationResponse> {
-    return serviceInstances.idpService.get(toLogicUrl(MFA_CONFIG_ENDPOINTS.GET), undefined, { absoluteUrl: true });
+    return serviceInstances.idpService.get(toIamUrl(MFA_CONFIG_ENDPOINTS.GET), undefined, { absoluteUrl: true });
   }
 
   saveMFAConfiguration(
     payload: IMFAConfigurationSavePayload,
   ): Promise<IMFAConfigurationSaveResponse> {
-    return serviceInstances.idpService.post(MFA_CONFIG_ENDPOINTS.SAVE, payload);
+    return serviceInstances.idpService.post(toIamUrl(MFA_CONFIG_ENDPOINTS.SAVE), payload, undefined, { absoluteUrl: true });
   }
 
   generateUserMfaOTP(payload: IGenerateUserMFA_OtpPayload): Promise<IGenerateUserMFA_OtpResponse> {
-    return serviceInstances.idpService.post(toLogicUrl(MFA_ENDPOINTS.GENERATE_OTP), payload, undefined, { absoluteUrl: true });
+    return serviceInstances.idpService.post(toIamUrl(MFA_ENDPOINTS.GENERATE_OTP), payload, undefined, { absoluteUrl: true });
   }
 
   configureUserMFA(payload: IConfigureUserMFAPayload): Promise<IConfigureUserMFAResponse> {
@@ -41,21 +41,21 @@ export class MFAService {
   }
   setupUserTotp(payload: ISetupUserTotpPayload): Promise<ISetupUserTotpResponse> {
     return serviceInstances.idpService.get(
-      toLogicUrl(`${MFA_ENDPOINTS.SETUP_TOTP}?UserId=${payload.id}`),
+      toIamUrl(`${MFA_ENDPOINTS.SETUP_TOTP}?UserId=${payload.id}`),
       undefined,
       { absoluteUrl: true },
     );
   }
 
   verifyOtp(payload: IVerifyMfaOtpPayload): Promise<IVerifyMfaOtpResponse> {
-    return serviceInstances.idpService.post(toLogicUrl(MFA_ENDPOINTS.VERIFY_OTP), payload, undefined, { absoluteUrl: true });
+    return serviceInstances.idpService.post(toIamUrl(MFA_ENDPOINTS.VERIFY_OTP), payload, undefined, { absoluteUrl: true });
   }
 
   resendOtp(payload: IResendMfaOtpPayload): Promise<IResendMfaOtpResponse> {
     return serviceInstances.idpService.post(MFA_ENDPOINTS.RESEND_OTP, payload.mfaId);
   }
   disableMFA(payload: IDisableMFAPayload): Promise<IDisableMFAResponse> {
-    return serviceInstances.idpService.post(toLogicUrl(MFA_ENDPOINTS.DISABLE_MFA), payload, undefined, { absoluteUrl: true });
+    return serviceInstances.idpService.post(toIamUrl(MFA_ENDPOINTS.DISABLE_MFA), payload, undefined, { absoluteUrl: true });
   }
 }
 
