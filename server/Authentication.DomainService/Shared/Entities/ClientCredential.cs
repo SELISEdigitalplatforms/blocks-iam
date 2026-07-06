b7ec9a39f51dd8b4732f7d1bb63b387e13df5310
mostafizSelise <mostafizur.rahman@selisegroup.com>
2026-07-07 03:58:36 +0600

    updated

--- server/Authentication.DomainService/Shared/Entities/ClientCredential.cs
+++ server/Authentication.DomainService/Shared/Entities/ClientCredential.cs
@@ -6,6 +6,7 @@ namespace Authentication.DomainService.Entities
     {
         public string? Name { get; set; }
         public string? ClientSecret { get; set; }
+        public int AccessTokenValidForNumberMinutes { get; set; } = 5;
         public List<string> Roles { get; set; } = new List<string>();
         public List<string> Permissions { get; set; } = new List<string>();
         public bool IsActive { get; set; }
