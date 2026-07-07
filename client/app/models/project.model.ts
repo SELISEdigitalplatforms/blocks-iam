2abc40185e6e0991753f7069f786cce358c364b2
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-07-07 13:11:17 +0600

    update: blocks-kit package version and remove unnecessary files

--- client/app/models/project.model.ts
+++ client/app/models/project.model.ts
@@ -1,24 +1,25 @@
-export interface IProject {
-  itemId: string;
-  createdDate: string;
-  lastUpdatedDate: string;
-  createdBy: string;
-  lastUpdatedBy: string;
-  organizationIds: string[];
-  tags: string[];
-  name: string;
-  applicationDomain: string;
-  customDomain: string;
-  isProduction: true;
-  tenantId: string;
-  isCookieEnable: boolean;
-  isDomainVerified: boolean;
-  cookieDomain: string;
-  isDisabled: boolean;
-  environment: string;
-  tenantGroupId: string;
-  tenantSlug: string;
-}
+// export interface IProject {
+//   itemId: string;
+//   createdDate: string;
+//   lastUpdatedDate: string;
+//   createdBy: string;
+//   lastUpdatedBy: string;
+//   organizationIds: string[];
+//   tags: string[];
+//   name: string;
+//   applicationDomain: string;
+//   customDomain: string;
+//   isProduction: true;
+//   tenantId: string;
+//   isCookieEnable: boolean;
+//   isDomainVerified: boolean;
+//   cookieDomain: string;
+//   isDisabled: boolean;
+//   environment: string;
+//   tenantGroupId: string;
+//   tenantSlug: string;
+// }
+import type {IProject } from '@seliseblocks/blocks-kit'
 
 export interface IProjectGroup {
   tenantGroupId: string;
@@ -35,3 +36,4 @@ export interface IGetProjectResponse {
   data: IProject;
   errors: unknown | null;
 }
+export type {IProject}
