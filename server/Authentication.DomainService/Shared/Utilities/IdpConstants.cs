b47602bc785338c99e6a26d5f46e22f1b22ebed9
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-07-08 16:05:34 +0600

    updated

--- server/Authentication.DomainService/Shared/Utilities/IdpConstants.cs
+++ server/Authentication.DomainService/Shared/Utilities/IdpConstants.cs
@@ -6,7 +6,7 @@ namespace Authentication.DomainService.Utilities
     {
         public const string TenantTokenPublicCertificateCachePrefix = "tetocertpublic::";
         public const string AuthenticationQueue = "blocks_idp_authentication_listener";
-        public const string IamQueue = "blocks_idp_iam_listener_local";
+        public const string IamQueue = "blocks_iam_listener_idp";
         public const string IamOrgQueue = "blocks_idp_iam_org_listener";
         public const string MailQueue = "blocks_email_listener";
         public const string MfaQueueName = "blocks_idp_mfa_listener";
