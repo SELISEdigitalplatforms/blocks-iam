c093f65186d4f5f4f09ce7a92645dac78902df61
afiapreety <afia.preety@selisegroup.com>
2026-07-08 15:14:02 +0600

    fix: pushing with build issue

--- client/app/components/project-list/project-list.tsx
+++ client/app/components/project-list/project-list.tsx
@@ -99,10 +99,7 @@ export function ProjectList({ collapsed = false }: { collapsed?: boolean }) {
             .filter((project) => project.itemId !== selectedProject?.itemId)
             .slice(0, 5)
             .map((project) => (
-              <DropdownMenuItem
-                key={project.itemId}
-                onSelect={() => handleProjectSelect({ ...project, applications: [] } as IProject)}
-              >
+             <DropdownMenuItem key={project.itemId} onSelect={() => handleProjectSelect(project)}>
                 <span>{project.name}</span>
               </DropdownMenuItem>
             ))
