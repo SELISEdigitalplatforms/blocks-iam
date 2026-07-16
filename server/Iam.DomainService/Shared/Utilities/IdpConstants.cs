01d814dd88d8d6b0e1263fee28fb1b6fa3179240
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-07-16 20:29:15 +0600

    constant name change

--- server/Iam.DomainService/Shared/Utilities/IdpConstants.cs
+++ server/Iam.DomainService/Shared/Utilities/IdpConstants.cs
@@ -34,7 +34,8 @@ namespace Iam.DomainService.Utilities
         #region Providers / Protocols
 
         public const string BlocksProviderName = "blocks-iam";
-        public const string BlocksProviderType = "blocks-oidc";
+        public const string BlocksProviderType = "blocks";
+        public const string BlocksOidcProviderType = "blocks-oidc";
         public const string OidcProtocol = "oidc";
 
         #endregion
