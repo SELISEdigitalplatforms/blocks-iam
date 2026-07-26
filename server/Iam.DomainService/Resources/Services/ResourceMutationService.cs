8b6f51328c60861a4939b7f4ba9f305b60229b30
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-07-26 18:39:47 +0600

    roles issue resolve

--- server/Iam.DomainService/Resources/Services/ResourceMutationService.cs
+++ server/Iam.DomainService/Resources/Services/ResourceMutationService.cs
@@ -1,4 +1,4 @@
-﻿using Blocks.Genesis;
+using Blocks.Genesis;
 using FluentValidation;
 using Iam.DomainService.Dtos;
 using Iam.DomainService.Entities;
@@ -1115,7 +1115,7 @@ namespace Iam.DomainService.Resources
                     {
                         await _resourceRepository.UpdateRolePermissionByIdsAsync(
                             command.Slug,
-                            addPermissionIds);
+                            addPermissionIds,orgId);
                     }
                 }
 
