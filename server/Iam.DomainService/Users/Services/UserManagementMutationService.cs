e0826a8f023ccb2ada5551a39cda5409617bfde9
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-07-31 23:59:55 +0600

    fix user access management saving issue

--- server/Iam.DomainService/Users/Services/UserManagementMutationService.cs
+++ server/Iam.DomainService/Users/Services/UserManagementMutationService.cs
@@ -748,23 +748,23 @@ namespace Iam.DomainService.Users
                 return new BaseMutationResponse();
             }
 
-            await SendEvent(user.ItemId, MutationEventType.Update);
+            //await SendEvent(user.ItemId, MutationEventType.Update);
 
-            await _userActivityDispatcher.SendUserActivityAsync(new UserActivityEvent
-            {
-                UserId = user.ItemId,
-                Category = UserActivityCategory.Resource,
-                Event = isAddToOrganization ? "USER_ACCESS_UPDATED" : "USER_ACCESS_REVOKED",
-                Source = "iam-user-access-control",
-                Entity = "User",
-                EntityId = user.ItemId,
-                Metadata = new Dictionary<string, string>
-                {
-                    { "organizationId", organizationId ?? string.Empty },
-                    { "rolesAdded", string.Join(",", command.Roles ?? new List<string>()) },
-                    { "permissionsAdded", string.Join(",", command.Permissions ?? new List<string>()) }
-                }
-            });
+            //await _userActivityDispatcher.SendUserActivityAsync(new UserActivityEvent
+            //{
+            //    UserId = user.ItemId,
+            //    Category = UserActivityCategory.Resource,
+            //    Event = isAddToOrganization ? "USER_ACCESS_UPDATED" : "USER_ACCESS_REVOKED",
+            //    Source = "iam-user-access-control",
+            //    Entity = "User",
+            //    EntityId = user.ItemId,
+            //    Metadata = new Dictionary<string, string>
+            //    {
+            //        { "organizationId", organizationId ?? string.Empty },
+            //        { "rolesAdded", string.Join(",", command.Roles ?? new List<string>()) },
+            //        { "permissionsAdded", string.Join(",", command.Permissions ?? new List<string>()) }
+            //    }
+            //});
 
             _logger.LogInformation("Update User Access Control end -- Success");
             return new BaseMutationResponse
